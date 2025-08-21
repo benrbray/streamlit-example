f6efafca45d1cafce927442853a9eafcd5feb195
Benjamin R. Bray <benrbray@gmail.com>
2025-08-21 22:17:24 +0900

    update

--- pages/gif.py
+++ pages/gif.py
@@ -7,7 +7,10 @@ st.markdown("# Giphy API")
 query = st.text_input("Enter a search query for the `giphy` API:")
 
 url = "https://api.giphy.com/v1/gifs/search"
-params = { "api_key" : st.secrets.api_key, "q": query }
+params = {
+    "api_key" : st.secrets.api_key,
+    "q": query
+}
 response = requests.get(url=url, params=params).json()
 
 while not query:
@@ -16,8 +19,12 @@ while not query:
 gif_data = response["data"][np.random.randint(0,10)]
 gif_url = gif_data["images"]["original"]["url"]
 
-# st.write(gif_data)
-
+st.markdown("### Method 1")
 st.markdown(f"![]({gif_url})")
 
+st.markdown("### Method 2")
 st.write(f'<img height=600 src="{gif_url}"/>', unsafe_allow_html=True)
+
+st.markdown("### JSON Data")
+with st.expander(label="Expand Me!"):
+    st.write(gif_data)
